3834f8713ec1d516344788c176cbbd4c0dd2d5a3
Anatolie Lupacescu <anatolie.lupacescu@gamesys.com.uk>
2016-02-05 16:29:24 +0000

    exception handling testing

--- src/test/java/camel/sample/JettyCamelTest.java
+++ src/test/java/camel/sample/JettyCamelTest.java
@@ -1,10 +1,11 @@
 package camel.sample;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.processor.loadbalancer.CircuitBreakerLoadBalancer;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 /**
@@ -14,13 +15,23 @@ import org.junit.Test;
  */
 public class JettyCamelTest extends BaseJettyTest {
 
+	private static final int REDELIVERY_COUNT = 3;
+	private static final int CALL_COUNT = 3;
+	private final Logger LOGGER = Logger.getLogger(JettyCamelTest.class);
+	private final AtomicInteger atomicInteger = new AtomicInteger(0);
+
 	@Test
 	public void testSendWithSOTimeoutTimeout() throws Exception {
-		MockEndpoint mock = getMockEndpoint("mock:result");
-		mock.expectedMessageCount(1);
-		String out = template.requestBody("jetty:http://0.0.0.0:{{port}}/myservice", null, String.class);
-		assertEquals("Bye World", out);
-		assertMockEndpointsSatisfied();
+		// MockEndpoint mock = getMockEndpoint("mock:result");
+		// mock.expectedMessageCount(1);
+		for (int i = 0; i < CALL_COUNT; i++) {
+			try {
+			template.requestBody("jetty:http://0.0.0.0:{{port}}/myservice", null, String.class);
+			} catch (Exception e){}
+		}
+		assertEquals(CALL_COUNT * (REDELIVERY_COUNT + 1), atomicInteger.get());
+		// assertEquals("Bye World", out);
+		// assertMockEndpointsSatisfied();
 	}
 
 	@Override
@@ -29,28 +40,30 @@ public class JettyCamelTest extends BaseJettyTest {
 			@Override
 			public void configure() throws Exception {
 
+				final CircuitBreakerLoadBalancer circuitBreaker = new CircuitBreakerLoadBalancer(
+						Arrays.asList(Exception.class));
+				circuitBreaker.setHalfOpenAfter(10000L);
+				circuitBreaker.setThreshold(2);
+
 				errorHandler(defaultErrorHandler().maximumRedeliveries(3));
-
+				
+/*
 				onException(IllegalStateException.class)
-					.handled(true)
-					.maximumRedeliveries(2)
-					.to("log:camel.sample?level=ERROR&showCaughtException=true&showStackTrace=true&multiline=true")
-					.log(LoggingLevel.INFO, "processing exception");
-
+				.handled(true)
+				.maximumRedeliveries(REDELIVERY_COUNT).setBody(constant("lol"));
+*/
 				from("jetty://http://0.0.0.0:{{port}}/myservice")
-					.log(LoggingLevel.INFO,"£ to direct error")
-					.to("direct:error")
-					.transform().constant("Bye World").to("mock:result");
+					.transform().constant("Bye World")
+					 .loadBalance(circuitBreaker)
+					 	.to("direct:error")
+					.end()
+					.to("mock:result");
 
-				AtomicInteger atomicInteger = new AtomicInteger(0);
-
-				from("direct:error")
-					.log(LoggingLevel.INFO, "£ throwing Exception")
-					.process(exchange -> {
-						if (atomicInteger.incrementAndGet() < 3) {
-							throw new IllegalStateException();
-						}
-					});
+				from("direct:error").process(exchange -> {
+					atomicInteger.incrementAndGet();
+					LOGGER.debug("Throwing exception! circuit breaker state " + circuitBreaker.getState());
+					throw new IllegalStateException();
+				});
 			}
 		};
 	}
