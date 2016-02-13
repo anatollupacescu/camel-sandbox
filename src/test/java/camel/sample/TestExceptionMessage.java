6cab8deced3daa5981279636870442fe95a1681d
Anatolie Lupacescu <anatolie.lupacescu@gamesys.com.uk>
2016-02-13 17:52:32 +0000

    added a new test for exception handling

---
+++ src/test/java/camel/sample/TestExceptionMessage.java
@@ -0,0 +1,71 @@
+package camel.sample;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.SocketException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class TestExceptionMessage extends CamelTestSupport {
+
+	private static final String DIRECT_INPUT = "direct:input";
+
+	@Produce(uri = DIRECT_INPUT)
+	private ProducerTemplate testProducer;
+
+	private AtomicInteger counter = new AtomicInteger(0);
+	
+	@Test
+	public void testRoute2() throws InterruptedException {
+		Exchange ex = sendMessage(DIRECT_INPUT, 0);
+		String body = ex.getIn().getBody(String.class);
+		System.out.println(body);
+	}
+
+	@Override
+	protected RouteBuilder createRouteBuilder() {
+		return new RouteBuilder() {
+			public void configure() throws Exception {
+
+				onException(Exception.class)
+				.handled(true)
+				.process(ex -> {
+					System.out.println(ex.getIn().getBody());
+				});
+				
+				onException(IOException.class)
+				.handled(true)
+				.process(ex -> {
+					System.out.println(ex.getIn().getBody());
+				});
+				
+				onException(SocketException.class)
+				.maximumRedeliveries(2)
+				.handled(true)
+				.process(ex -> {
+					System.out.println(counter.get());
+				});	
+				
+				from(DIRECT_INPUT)
+					.routeId("testRoute")
+					.process( ex -> { 
+						counter.incrementAndGet();
+						throw new ConnectException();
+					})
+					.end();
+			}
+		};
+	}
+
+	private Exchange sendMessage(final String endpoint, final Object body) {
+		return template.send(endpoint, exchange -> {
+			exchange.getIn().setBody(body);
+		});
+	}
+}
