69c512e0739ead1b9c2b62b5904d3ab833e04455
Anatolie Lupacescu <anatollupacescu@gmail.com>
2016-11-07 21:06:37 +0200

    inc version

--- src/main/java/camel/sample/MainApp.java
+++ src/main/java/camel/sample/MainApp.java
@@ -12,11 +12,9 @@ public class MainApp {
      */
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        main.enableHangupSupport();
         main.addRouteBuilder(new FileProcessingRouteBuilder());
         main.addRouteBuilder(new StickyLoadBalancerSedaRouteBuilder());
         main.addRouteBuilder(new MockHttpRouteBuilder());
-
         main.run(args);
     }
 }
